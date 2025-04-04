cda1db9bc440daaad0106e80962ea3cafaf80d69
ymd <ymd20060720@gmail.com>
2025-04-04 11:08:22 +0200

    Imageコンポーネントを分割

--- images.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "*.png" {
-  const value: ImageSourcePropType;
-  export = value;
-}
--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,22 +1,17 @@
 import { type FC } from "react";
-import {
-  Image,
-  StyleSheet,
-  View,
-  type ImageSourcePropType,
-} from "react-native";
+import { StyleSheet, View, type ImageSourcePropType } from "react-native";
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
 
-import PlaceholderImage from "../assets/images/background-image.png";
+import PlaceholderImage from "@/assets/images/background-image.png";
+import { ImageViewer } from "@/components/image-viewer";
 
 const App: FC = () => {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image
-          source={PlaceholderImage as ImageSourcePropType}
-          style={styles.image}
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage as ImageSourcePropType}
         />
       </View>
       <StatusBar style="auto" />
@@ -33,11 +28,6 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
 });
 
