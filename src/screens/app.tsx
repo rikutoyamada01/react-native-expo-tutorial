1f743ca86a4cc8341d8d24b801703253ad1fda16
ymd <ymd20060720@gmail.com>
2025-04-04 12:07:28 +0200

    ボタンにアイコンを追加

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -16,7 +16,7 @@ const App: FC = () => {
         />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="写真を選択"></Button>
+        <Button theme="primary" label="写真を選択"></Button>
         <Button label="この写真を使用"></Button>
       </View>
       <StatusBar style="auto" />
