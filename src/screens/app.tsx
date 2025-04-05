0c4b40b0ab4864e6d11207c131ebb328b2ba3600
ymd <ymd20060720@gmail.com>
2025-04-05 11:17:32 +0200

    選択された絵文字を表示

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/button";
 import { CircleButton } from "@/components/circle-button";
 import { EmojiList } from "@/components/emoji-list";
 import { EmojiPicker } from "@/components/emoji-picker";
+import { EmojiSticker } from "@/components/emoji-sticker";
 import { IconButton } from "@/components/icon-button";
 import { ImageViewer } from "@/components/image-viewer";
 
@@ -57,6 +58,9 @@ const App: FC = () => {
           placeholderImageSource={PlaceholderImage as ImageSourcePropType}
           selectedImage={selectedImage}
         />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
       </View>
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
