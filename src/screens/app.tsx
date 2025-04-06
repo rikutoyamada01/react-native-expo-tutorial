6f96b69936929758abaff96f06fad9939ad8a5ae
ymd <ymd20060720@gmail.com>
2025-04-06 09:10:44 +0200

    写真へのアクセス権限の取得

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, View, type ImageSourcePropType } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 import * as ImagePicker from "expo-image-picker";
+import * as Medialibrary from "expo-media-library";
 import { StatusBar } from "expo-status-bar";
 
 import PlaceholderImage from "@/assets/images/background-image.png";
@@ -21,6 +22,11 @@ const App: FC = () => {
   const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | null>(
     null,
   );
+  const [status, requestPermission] = Medialibrary.usePermissions();
+
+  if (status === null) {
+    void requestPermission();
+  }
 
   const onModalClose = () => {
     setIsModalVisible(false);
